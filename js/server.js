bfd979631f829468a75e5234c5fa32f4d1d25959
Amandeep <amandeep.cs@gmail.com>
2017-10-29 02:10:41 +0530

    L1 with response-body lib

--- js/server.js
+++ js/server.js
@@ -1,6 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 
 var app = express();
+
+app.set('view engine', 'ejs');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.listen(8090, function(){
 	console.log('node server started using express!');
@@ -9,4 +18,21 @@ app.listen(8090, function(){
 
 app.get('/', function(req, res){
 	res.send('Hello! node here...');
+});
+
+app.get('/greetings/:name', function(req, res){
+	res.send('Hello '+req.params.name);
+});
+
+app.get('/greetings/ejs/:name', function(req, res){
+	res.render('hello.ejs', {name: req.params.name});
+});
+
+app.get('/welcome', function(req, res){
+	res.render('welcome');
+});
+
+app.post('/details', function(req, res){
+	console.log(req.body);
+	res.send('recieved');
 });
