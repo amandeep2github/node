698354e54a1e09172bd2b6cfee79da419ea91aea
Amandeep <amandeep.cs@gmail.com>
2017-10-29 12:09:10 +0530

    with ejs, partials, request

--- js/server.js
+++ js/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser')
-
+var request = require('request');
 var app = express();
 
 app.set('view engine', 'ejs');
@@ -35,4 +35,27 @@ app.get('/welcome', function(req, res){
 app.post('/details', function(req, res){
 	console.log(req.body);
 	res.send('recieved');
+});
+
+app.get("/site/:name", function(req, res){
+	request("http://"+req.params.name, function(error, response, body){
+		console.log(response);
+	});
+	res.send('done');
+});
+
+app.get("/sample-json", function(req, res){
+	res.send('{"name":"amandeep", "age":38}');
+
+});
+app.get("/get-sample-json", function(req, res){
+	request("http://localhost:8090/sample-json", function(error, response, body){
+		var jsonObj = JSON.parse(body);
+		console.log(jsonObj);
+		res.send(jsonObj['name']);
+	});
+});
+
+app.get("/params-query-example/:param", function(req, res){
+	res.send('You entered param - '+req.params.param+' and query - '+req.query.query);
 });
